cd80f8774fa5e53d8e522f9d0c17adabff161acb
Samuel <samuelpeybernes33@gmail.com>
2024-04-13 13:21:01 +0200

    remove socket io

--- src/app/components/ticket-shop/ticket-shop.component.ts
+++ src/app/components/ticket-shop/ticket-shop.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
-import { webSocketService } from '../../services/webSocketService.service';
-import { Price as PriceData } from '../../interfaces/price.interface';
+
 
 @Component({
   selector: 'ticket-shop',
@@ -11,13 +10,5 @@ import { Price as PriceData } from '../../interfaces/price.interface';
   styleUrl: './ticket-shop.component.css'
 })
 export class TicketShopComponent {
-  muchPrices: PriceData[] = [];
-  constructor(private priceService: webSocketService) {}
 
-  ngOnInit(): void {
-    this.priceService.listenForPrices().subscribe((data) => {
-      console.log(data);
-      // Vous pouvez traiter les données ici, par exemple, les afficher dans votre interface utilisateur
-    });
-  }
 }
