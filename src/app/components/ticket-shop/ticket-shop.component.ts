d31d57de9479e3771c5dd6f52c317e5b23e55c7a
Samuel <samuelpeybernes33@gmail.com>
2024-04-13 13:19:03 +0200

    try debug cors websocket

--- src/app/components/ticket-shop/ticket-shop.component.ts
+++ src/app/components/ticket-shop/ticket-shop.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import { webSocketService } from '../../services/webSocketService.service';
+import { Price as PriceData } from '../../interfaces/price.interface';
 
 @Component({
   selector: 'ticket-shop',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './ticket-shop.component.html',
   styleUrl: './ticket-shop.component.css'
 })
 export class TicketShopComponent {
+  muchPrices: PriceData[] = [];
+  constructor(private priceService: webSocketService) {}
 
+  ngOnInit(): void {
+    this.priceService.listenForPrices().subscribe((data) => {
+      console.log(data);
+      // Vous pouvez traiter les données ici, par exemple, les afficher dans votre interface utilisateur
+    });
+  }
 }
