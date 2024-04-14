83e22d90aa998a0e7ef957dc6a16b4f6e294cb1b
Samuel <samuelpeybernes33@gmail.com>
2024-04-14 21:58:12 +0200

    improve responsive

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { SidebarModule } from 'primeng/sidebar';
 
 @Component({
   selector: 'nav-bar',
   standalone: true,
-  imports: [RouterLink, RouterModule, ButtonModule],
+  imports: [RouterLink, RouterModule, ButtonModule, SidebarModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
-
+  sidebarVisible: boolean = false;
 }
