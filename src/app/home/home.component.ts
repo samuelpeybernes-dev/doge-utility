7aed11ad847fe8ba1f8e01bb846ee9b8073aeb0e
Samuel <72594810+samuelpeybernes-dev@users.noreply.github.com>
2024-10-13 22:19:48 +0200

    add btn

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { MuchPriceComponent } from './much-price/much-price.component';
 import { WowCexComponent } from './wow-cex/wow-cex.component';
 import { HeaderComponent } from '../shared/header/header.component';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [HeaderComponent, MuchPriceComponent, WowCexComponent],
+  imports: [HeaderComponent, MuchPriceComponent, WowCexComponent, ButtonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  imageUrl: string = "https://cryptoast.fr/wp-content/uploads/2024/04/jo-2024-dogecoin-doge-devient-partenaires-stade-france.jpg"
+  imageUrl: string =
+    'https://cryptoast.fr/wp-content/uploads/2024/04/jo-2024-dogecoin-doge-devient-partenaires-stade-france.jpg';
+
+  constructor(private router: Router) {}
+
+  toUrl() {
+    this.router.navigateByUrl('/bourse');
+  }
 }
